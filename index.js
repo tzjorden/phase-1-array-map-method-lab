97505bf9939fefde1b52615e066361af397cb00f
Tenzin Jorden <tenden96@gmail.com>
2021-12-16 20:56:57 -0800

    commit tree 851885d08cdc6e8d16f58b96b2d5cd0febf676d7

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(" ")
+      .map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+
+  });
+};
